462ca9425bf3b92fabd9338223d89163a0890f3f
dongwan999 <dongwan999@naver.com>
2021-10-31 19:11:49 +0900

    close #6 - feature - Firestore Upload Feature

--- app/src/main/java/com/devwan/plateofselfreflection/AllPlateFragment.kt
+++ app/src/main/java/com/devwan/plateofselfreflection/AllPlateFragment.kt
@@ -1,14 +1,25 @@
 package com.devwan.plateofselfreflection
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.SavedStateViewModelFactory
 
 class AllPlateFragment : Fragment() {
+
+    private lateinit var mContext: Context
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
+    }
 
     private val viewModel : AllPlateViewModel by viewModels(
         factoryProducer = { SavedStateViewModelFactory(activity?.application,this) }
@@ -24,6 +35,12 @@ class AllPlateFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val rootView : View = inflater.inflate(R.layout.fragment_all_plate, container, false)
+        val btnCreateUploadActivity = rootView.findViewById<ImageButton>(R.id.btn_create_upload_activity)
+
+        btnCreateUploadActivity.setOnClickListener {
+            val intent = Intent(mContext, UploadPlateActivity::class.java)
+            startActivity(intent)
+        }
 
         return rootView
     }
