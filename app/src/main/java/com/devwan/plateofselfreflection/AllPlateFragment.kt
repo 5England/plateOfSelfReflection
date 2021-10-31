e3aabacddd8cd13973d1d774a4cc66b2f13fa321
dongwan999 <dongwan999@naver.com>
2021-11-01 03:11:44 +0900

    recyclerView - viewHolder 작업

--- app/src/main/java/com/devwan/plateofselfreflection/AllPlateFragment.kt
+++ app/src/main/java/com/devwan/plateofselfreflection/AllPlateFragment.kt
@@ -11,14 +11,20 @@ import android.widget.*
 import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.SavedStateViewModelFactory
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentSnapshot
 
-class AllPlateFragment : Fragment() {
+class AllPlateFragment : Fragment(), AllPlateAdapter.OnGetListType {
 
+    private lateinit var recyclerView : RecyclerView
     private lateinit var mContext: Context
     var isTimeListType = true
+
+    override fun isTimeTypeList(): Boolean {
+        return isTimeListType
+    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -39,9 +45,15 @@ class AllPlateFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val rootView : View = inflater.inflate(R.layout.fragment_all_plate, container, false)
+        val recyclerView = rootView.findViewById<RecyclerView>(R.id.recyclerView_allPlate)
         val btnCreateUploadActivity = rootView.findViewById<ImageButton>(R.id.btn_createUploadActivity)
         val btnSetListType = rootView.findViewById<ImageButton>(R.id.btn_setListType)
         val btnGetList = rootView.findViewById<Button>(R.id.btn_getList)
+
+        recyclerView.apply {
+            this.layoutManager = LinearLayoutManager(activity?.application)
+            this.adapter = com.devwan.plateofselfreflection.AllPlateAdapter(emptyList(), mContext)
+        }
 
         btnCreateUploadActivity.setOnClickListener {
             val intent = Intent(mContext, UploadPlateActivity::class.java)
@@ -70,7 +82,77 @@ class AllPlateFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         allPlateViewModel.plate.observe(viewLifecycleOwner){
-
+            (recyclerView.adapter as AllPlateAdapter).setData(it)
         }
+    }
+}
+
+class AllPlateAdapter(private var plateList: List<DocumentSnapshot>, private var mContext : Context) :
+        RecyclerView.Adapter<AllPlateAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var isOvercome: ImageView? = null
+        var title: TextView? = null
+        var mainText: TextView? = null
+        var uploadTime: TextView? = null
+        var like: TextView? = null
+
+        init {
+            isOvercome = view.findViewById(R.id.img_isOvercome)
+            title = view.findViewById(R.id.text_title)
+            mainText = view.findViewById(R.id.text_mainText)
+            uploadTime = view.findViewById(R.id.text_uploadTime)
+            like = view.findViewById(R.id.text_like)
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+                .inflate(R.layout.card_plate, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        bindData(viewHolder, plateList[position])
+    }
+
+    override fun getItemCount() = plateList.size
+
+    interface OnGetListType{ fun isTimeTypeList() : Boolean }
+    private val onGetListType : OnGetListType = mContext as OnGetListType
+
+    fun setData(newData: List<DocumentSnapshot>) {
+        if(onGetListType.isTimeTypeList()){
+            plateList = getTimeTypeList(newData)
+        }else{
+            plateList = getLikeTypeList(newData)
+        }
+
+        notifyDataSetChanged()
+    }
+
+    private fun bindData(viewHolder: ViewHolder, plateDocumentSnapshot: DocumentSnapshot) {
+        val title: String = plateDocumentSnapshot["title"].toString() ?: ""
+        val mainText: String = plateDocumentSnapshot["mainText"].toString() ?: ""
+        val like: String = plateDocumentSnapshot["like"].toString() ?: ""
+        val uploadTime: String = Plate.getUploadTimeText((plateDocumentSnapshot["uploadTime"] as Timestamp).toDate())
+        val isOvercome: Boolean = plateDocumentSnapshot["isOvercome"] as Boolean
+
+        viewHolder.title?.text = title
+        viewHolder.mainText?.text = mainText
+        viewHolder.like?.text = like
+        viewHolder.uploadTime?.text = uploadTime
+        if (isOvercome) viewHolder.isOvercome?.setImageResource(R.drawable.cardplate_icon_isovercome_true)
+
+        //layout 클릭 시 PlateActivity 생성, 인텐트로 데이터 전송
+    }
+
+    private fun getTimeTypeList(newData: List<DocumentSnapshot>) : List<DocumentSnapshot>{
+        return newData.sortedByDescending { (it["uploadTime"] as Timestamp).toDate() }
+    }
+
+    private fun getLikeTypeList(newData: List<DocumentSnapshot>) : List<DocumentSnapshot>{
+        return newData.sortedByDescending { (it["uploadTime"] as Timestamp).toDate() }.sortedByDescending { it["like"] as Long }
     }
 }
--- app/src/main/java/com/devwan/plateofselfreflection/Plate.kt
+++ app/src/main/java/com/devwan/plateofselfreflection/Plate.kt
@@ -1,6 +1,7 @@
 package com.devwan.plateofselfreflection
 
 import com.google.firebase.Timestamp
+import java.util.*
 
 data class Plate(val nickName : String = "(익명)",
                  val title : String,
@@ -9,4 +10,24 @@ data class Plate(val nickName : String = "(익명)",
                  val uploadTimestamp : Timestamp,
                  val isOvercome : Boolean = false,
                  val like : Long = 0,
-                 val LikeUidMap : Map<String, Boolean> = mapOf<String, Boolean>()){}
+                 val LikeUidMap : Map<String, Boolean> = mapOf<String, Boolean>()){
+
+    companion object {
+        fun getUploadTimeText(uploadDate : Date) : String {
+            val nowDate = Timestamp.now().toDate()
+            val uploadYear = uploadDate.year
+            val uploadMonth = uploadDate.month
+            val uploadDay = uploadDate.day
+
+            return if(nowDate.year == uploadYear){
+                if(nowDate.month == uploadMonth && nowDate.day == uploadDay) {
+                    uploadDate.hours.toString() + ":" + uploadDate.minutes.toString()
+                }else{
+                    uploadMonth.toString() + "/" + uploadDay.toString()
+                }
+            }else{
+                uploadYear.toString() + "/" + uploadMonth.toString() + "/" + uploadDay.toString()
+            }
+        }
+    }
+}
