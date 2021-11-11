c8ab49109b23587ae3b26eb19919916c0c2e8eff
dongwan999 <dongwan999@naver.com>
2021-11-11 17:28:49 +0900

    ReviewActivity.kt UI 작업 완료

--- app/src/main/java/com/devwan/plateofselfreflection/ReviewActivity.kt
+++ app/src/main/java/com/devwan/plateofselfreflection/ReviewActivity.kt
@@ -2,10 +2,25 @@ package com.devwan.plateofselfreflection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.devwan.plateofselfreflection.databinding.ActivityReviewBinding
+import com.devwan.plateofselfreflection.databinding.ActivityUploadPlateBinding
 
 class ReviewActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityReviewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review)
+        binding = ActivityReviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnFinishActivity.setOnClickListener {
+            finish()
+        }
+
+        binding.btnReview.setOnClickListener {
+
+        }
     }
 }
