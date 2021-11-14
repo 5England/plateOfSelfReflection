7a541e99fdd340de904718144041621fc62b2197
dongwan999 <dongwan999@naver.com>
2021-11-14 17:04:59 +0900

    InfoFragment UI 수정

--- app/src/main/java/com/devwan/plateofselfreflection/FirebaseAuthentication.kt
+++ app/src/main/java/com/devwan/plateofselfreflection/FirebaseAuthentication.kt
@@ -1,10 +1,14 @@
 package com.devwan.plateofselfreflection
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -46,7 +50,26 @@ class FirebaseAuthentication (private val activity : AppCompatActivity){
     }
 
     fun signOut() {
-        Firebase.auth.signOut()
-        signIn()
+        val user = FirebaseAuth.getInstance().currentUser
+        user?.let {
+            if (user.isAnonymous) {
+                val dlg = AlertDialog.Builder(activity, R.style.AlertDialogStyle)
+                dlg.apply {
+                    setTitle("로그아웃")
+                    setMessage("익명 사용자의 경우 기존 데이터가 유실될 수 있습니다.")
+                    setPositiveButton("취소", DialogInterface.OnClickListener { dialog, which ->
+
+                    })
+                    setNegativeButton("로그아웃", DialogInterface.OnClickListener { dialog, which ->
+                        Firebase.auth.signOut()
+                        signIn()
+                    })
+                    show()
+                }
+            } else {
+                Firebase.auth.signOut()
+                signIn()
+            }
+        }
     }
 }
