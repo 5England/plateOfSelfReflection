3754e8e120710259e217d0b53489c7c28056dac4
dongwan999 <dongwan999@naver.com>
2021-11-15 00:32:34 +0900

    close #38 linkGoogleAccount

--- app/src/main/java/com/devwan/plateofselfreflection/FirebaseAuthentication.kt
+++ app/src/main/java/com/devwan/plateofselfreflection/FirebaseAuthentication.kt
@@ -1,18 +1,21 @@
 package com.devwan.plateofselfreflection
 
 import android.app.Activity
-import android.app.AlertDialog
-import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
-import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
-class FirebaseAuthentication (private val activity : AppCompatActivity){
+
+class FirebaseAuthentication(private val activity: AppCompatActivity){
+
+    private lateinit var prevUid : String
+
+    //sign-in
 
     private val signInLauncher = activity.registerForActivityResult(
         FirebaseAuthUIActivityResultContract()
@@ -21,24 +24,14 @@ class FirebaseAuthentication (private val activity : AppCompatActivity){
     }
 
     fun signIn(){
-        // Choose authentication providers
         val providers = arrayListOf(
             AuthUI.IdpConfig.GoogleBuilder().build(),
             AuthUI.IdpConfig.AnonymousBuilder().build()
         )
-        // Create and launch sign-in intent
-        val signInIntent = AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .setLogo(R.drawable.icon_authlogo)
-            .setTheme(R.style.SignInTheme)
-            .setIsSmartLockEnabled(false)
-            .setLockOrientation(true)
-            .build()
+        val signInIntent = getSignInIntent(providers)
         signInLauncher.launch(signInIntent)
     }
 
-    // [START auth_fui_result]
     private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult){
         val response = result.idpResponse
         if (result.resultCode == Activity.RESULT_OK) {
@@ -49,27 +42,53 @@ class FirebaseAuthentication (private val activity : AppCompatActivity){
         }
     }
 
-    fun signOut() {
-        val user = FirebaseAuth.getInstance().currentUser
-        user?.let {
-            if (user.isAnonymous) {
-                val dlg = AlertDialog.Builder(activity, R.style.AlertDialogStyle)
-                dlg.apply {
-                    setTitle("로그아웃")
-                    setMessage("익명 사용자의 경우 기존 데이터가 유실될 수 있습니다.")
-                    setPositiveButton("취소", DialogInterface.OnClickListener { dialog, which ->
+    //link-GoogleAccount
 
-                    })
-                    setNegativeButton("로그아웃", DialogInterface.OnClickListener { dialog, which ->
-                        Firebase.auth.signOut()
-                        signIn()
-                    })
-                    show()
-                }
-            } else {
-                Firebase.auth.signOut()
-                signIn()
-            }
+    private val linkGoogleLauncher = activity.registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onLinkGoogleResult(res)
+    }
+
+    fun linkGoogleAccount() {
+        Firebase.auth.uid?.let {
+            prevUid = it
         }
+
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+        val signInIntent = getSignInIntent(providers)
+        linkGoogleLauncher.launch(signInIntent)
+    }
+
+    private fun onLinkGoogleResult(result: FirebaseAuthUIAuthenticationResult){
+        val response = result.idpResponse
+        if (result.resultCode == Activity.RESULT_OK) {
+            val onAuthServiceListener : OnAuthServiceListener = activity as OnAuthServiceListener
+            Firebase.auth.uid?.let {
+                onAuthServiceListener.updateNewUid(prevUid, it)
+            }
+        } else {
+            activity.finish()
+        }
+    }
+
+    //sign-out
+
+    fun signOut() {
+        Firebase.auth.signOut()
+        signIn()
+    }
+
+    private fun getSignInIntent(providers : ArrayList<AuthUI.IdpConfig>) : Intent {
+        return AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setLogo(R.drawable.icon_authlogo)
+            .setTheme(R.style.SignInTheme)
+            .setIsSmartLockEnabled(false)
+            .setLockOrientation(true)
+            .build()
     }
 }
--- app/src/main/java/com/devwan/plateofselfreflection/OnAuthServiceListener.kt
+++ app/src/main/java/com/devwan/plateofselfreflection/OnAuthServiceListener.kt
@@ -1,6 +1,10 @@
 package com.devwan.plateofselfreflection
 
+import com.google.firebase.auth.FirebaseUser
+
 interface OnAuthServiceListener {
     fun signOut()
     fun onSignInComplete()
+    fun linkGoogleAccount()
+    fun updateNewUid(prevUid : String, newUid : String)
 }
